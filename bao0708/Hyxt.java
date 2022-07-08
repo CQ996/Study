8e93d2b53628564dd0304d86aac7f3eac2f435b2
CQ996 <chengqian8417@gmail.com>
2022-07-08 11:18:21 +0800

    07.08

--- bao0708/Hyxt.java
+++ bao0708/Hyxt.java
@@ -10,7 +10,7 @@ public class Hyxt {
         int hyh=input.nextInt();
         System.out.print("请输入会员生日（月/日<用两位数表示>）：");
         String birthday=input.next();
-        System.out.print("请输入积分：）");
+        System.out.print("请输入积分：");
         int integral=input.nextInt();
         System.out.println("已录入的会员信息是：");
         System.out.println(hyh+"  "+birthday+"  "+integral);
