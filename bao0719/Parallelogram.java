41e0c18d524e0a734f3d352de0e83698ccfb0702
CQ996 <chengqian8417@gmail.com>
2022-07-19 16:12:08 +0800

    双循环输出平行四边形

---
+++ bao0719/Parallelogram.java
@@ -0,0 +1,22 @@
+package bao0719;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName Parallelogram
+ * @Description 双循环输出平行四边形
+ * @Author CQ
+ * @Date 2022/7/19 15:25
+ * @Version 1.0
+ */
+public class Parallelogram {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 5-i; j >=1 ; j--) {
+                System.out.print(" ");
+            }
+            System.out.println("*****");
+        }
+    }
+}
