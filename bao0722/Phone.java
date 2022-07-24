3e32471861ffb4c9a069758d51bfd95f474a50fa
CQ996 <chengqian8417@gmail.com>
2022-07-24 14:26:51 +0800

    手机类里用return返回字符串

---
+++ bao0722/Phone.java
@@ -0,0 +1,15 @@
+package bao0722;
+
+/**
+ * @ClassName Phone
+ * @Description 手机类里用return返回字符串
+ * @Author CQ
+ * @Date 2022/7/24 13:47
+ * @Version 1.0
+ */
+public class Phone {
+    String brand="诺基亚";
+    public String show(){
+        return brand+"手机可以下载音乐，播放这些音乐，还可以进行充电。";
+    }
+}
---
+++ bao0722/PhoneTest.java
@@ -0,0 +1,15 @@
+package bao0722;
+
+/**
+ * @ClassName PhoneTest
+ * @Description 手机类测试手：用return返回字符串
+ * @Author CQ
+ * @Date 2022/7/24 13:47
+ * @Version 1.0
+ */
+public class PhoneTest {
+    public static void main(String[] args) {
+        Phone phone=new Phone();
+        System.out.println(phone.show());
+    }
+}
