badf15392c0533eb203e54c8f7f341bb71b7f524
CQ996 <chengqian8417@gmail.com>
2022-07-24 14:14:01 +0800

    用*打印矩形

---
+++ bao0720/Rectangle009.java
@@ -0,0 +1,19 @@
+package bao0720;
+
+/**
+ * @ClassName Rectangle
+ * @Description 用*打印矩形
+ * @Author CQ
+ * @Date 2022/7/20 10:58
+ * @Version 1.0
+ */
+public class Rectangle009 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <=5 ; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
