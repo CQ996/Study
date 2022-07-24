a1ae026a1dc7654bbc0c6b64a96fb184b5067662
CQ996 <chengqian8417@gmail.com>
2022-07-24 14:22:30 +0800

    显示行星——测试——调用行星类2、行星类1

---
+++ bao0721/PlanetTest2.java
@@ -0,0 +1,16 @@
+package bao0721;
+
+/**
+ * @ClassName TestPlanet2
+ * @Description 显示行星——测试——调用行星类2、行星类1
+ * @Author CQ
+ * @Date 2022/7/21 15:45
+ * @Version 1.0
+ */
+public class PlanetTest2 {
+    public static void main(String[] args) {
+        PlanetTest planetTest =new PlanetTest();
+        planetTest.test();
+
+    }
+}
