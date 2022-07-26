5546216a5136f9ad190b3eaf3cdac30554f41a81
CQ996 <chengqian8417@gmail.com>
2022-07-26 14:18:41 +0800

    带参方法——定义榨汁机类

---
+++ bao0725/Fruit.java
@@ -0,0 +1,15 @@
+package bao0725;
+
+/**
+ * @ClassName Fruit
+ * @Description 带参方法——定义榨汁机类
+ * @Author CQ
+ * @Date 2022/7/25 16:06
+ * @Version 1.0
+ */
+public class Fruit {
+    public String zhazhi(String fruit){
+        String a="你要的"+fruit+"汁";
+        return a;
+    }
+}
---
+++ bao0725/FruitTest.java
@@ -0,0 +1,17 @@
+package bao0725;
+
+/**
+ * @ClassName FruitTest
+ * @Description TODO
+ * @Author CQ
+ * @Date 2022/7/25 16:06
+ * @Version 1.0
+ */
+public class FruitTest {
+    public static void main(String[] args) {
+        Fruit shuiguo=new Fruit();
+        String b=shuiguo.zhazhi("苹果");
+        System.out.println(b);
+
+    }
+}
