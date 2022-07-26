93c4c082106a535ea12026eae661fc1e28f68ba1
CQ996 <chengqian8417@gmail.com>
2022-07-26 14:36:48 +0800

    游记类——通过带参方法 循环添加游记标题 再输出

--- bao0725/AddTravels.java
+++ bao0725/AddTravels.java
@@ -1,5 +1,7 @@
 package bao0725;
 
+import java.util.Scanner;
+
 /**
  * @ClassName AddTravels
  * @Description 游记类——通过带参方法 循环添加游记标题 再输出
@@ -10,11 +12,23 @@ package bao0725;
 public class AddTravels {
     String answer;
     String[] titles=new String[4];
+    Scanner input=new Scanner(System.in);
+    public void youji(){
+        for (int i = 1; i <=4; i++) {
+            System.out.println("请输入游记标题：");
+            String title=input.next();
+            add(title);
+            if(i==4){
+                break;
+            }
+            System.out.println("是否继续发布游记？(y/n)");
+            answer=input.next();
+            if(!answer.equals("y")){
+                break;
+            }
+        }
+    }
 
-    /**
-     *
-     * @param title 定义游记标题参数
-     */
     public void add(String title){
         for (int i = 0; i < titles.length; i++) {
             if(titles[i]==null){
@@ -28,6 +42,24 @@ public class AddTravels {
         for (int i = 0; i < titles.length; i++) {
             if(titles[i]!=null){
                 System.out.println(titles[i]);
+            }
+        }
+    }
+
+    public void modify(){
+        while (2>1){
+            int i=0;
+            i++;
+            System.out.println("请输入需要修改的游记标题：");
+            String title=input.next();
+            if (title.equals(titles[i])){
+                System.out.println("请输入新标题：");
+                titles[i]=input.next();
+                System.out.println("*******修改结果*******");
+                System.out.println("游记标题修改成功");
+                break;
+            }else {
+                System.out.println("没有这个标题！");
             }
         }
     }
--- bao0725/AddTravelsTest.java
+++ bao0725/AddTravelsTest.java
@@ -11,40 +11,9 @@ import java.util.Scanner;
  */
 public class AddTravelsTest {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
         AddTravels addTravels=new AddTravels();
-        for (int i = 1; i <=4; i++) {
-            System.out.println("请输入游记标题：");
-            String title=input.next();
-            addTravels.add(title);
-            if(i==4){
-                break;
-            }
-            System.out.println("是否继续发布游记？(y/n)");
-            addTravels.answer=input.next();
-            if(!addTravels.answer.equals("y")){
-                break;
-            }
-        }
+            addTravels.youji();
             addTravels.show();
-
-            while (2>1){
-                int i=0;
-                i++;
-                System.out.println("请输入需要修改的游记标题：");
-                String title=input.next();
-                if (title.equals(addTravels.titles[i])){
-                    System.out.println("请输入新标题：");
-                    addTravels.titles[i]=input.next();
-                    System.out.println("*******修改结果*******");
-                    System.out.println("游记标题修改成功");
-                    break;
-                }else {
-                    System.out.println("没有这个标题！");
-                }
-            }
-
-
-
+            addTravels.modify();
     }
 }
