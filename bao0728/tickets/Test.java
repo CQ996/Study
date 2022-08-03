26e0f9bd011e00e0d9fee7fdf48acd03ad330b27
CQ996 <chengqian8417@gmail.com>
2022-08-03 09:02:16 +0800

    飞机航班价格表，用StringBuffer插入逗号

---
+++ bao0728/tickets/Test.java
@@ -0,0 +1,17 @@
+package bao0728.tickets;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author CQ
+ * @Date 2022/7/28 14:45
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        Flights flights=new Flights();
+        flights.login();
+        flights.buy();
+
+    }
+}
