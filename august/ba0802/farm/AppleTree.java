494d1e1a7636e067fcfe896182612811a155a7ea
CQ996 <chengqian8417@gmail.com>
2022-08-08 04:30:04 +0800

    星沐农场——有参、无参构造方法的使用、继承类、多态类，私有修饰符

--- august/ba0802/farm/AppleTree.java
+++ august/ba0802/farm/AppleTree.java
@@ -36,9 +36,24 @@ public class AppleTree extends Crop{
         this.brand=brand;
     }
 */
-   public AppleTree(String brand, int groTime){
-       this.setBrand(brand);//私有封装
-       super.setGroTime(groTime);
+    //嫁接新品种
+    public void grafting(String newBrand){
+        if(this.brand.equals(newBrand)){
+            System.out.println("同品种果树无需嫁接！");
+        }else {
+            this.brand=newBrand;
+            System.out.println("经过嫁接，"+super.getName()+"的品种变为："+this.brand);
+        }
+    }
+
+   public AppleTree(String name,int groTime,int havrsTime,int numsFruit,String status,boolean isHarvested,String brand){
+       this.name=name;
+       this.setGroTime(groTime);
+       this.setHavrsTime(havrsTime);
+       this.setNumsFruit(numsFruit);
+       this.setStatus(status);
+       this.brand=brand;
+       //super.setGroTime(groTime);
    }
    public AppleTree(String brand){
        this.setBrand(brand);//私有封装
@@ -61,19 +76,19 @@ public class AppleTree extends Crop{
 
     @Override//子类重写父类的方法，方法名需要相同，返回值类型需要一致或是其子类
     public void show(){
-        /*
+
         System.out.println("*****作物特性*****");
         System.out.println("你种植了"+name+",");
         System.out.println("成长期"+this.getGroTime()+"天，采摘期"+this.getHavrsTime()+"天，" +
                 "果实数量"+this.getNumsFruit()+"个，现在处于"+this.getStatus()+"，");
-        */
-        super.show();
+        //super.show();
         System.out.println(name+"属于水果作物，品种："+brand+"。");
     }
 
     @Override
     public void state() {
-        super.state();
+        /*super.state();*/
+        System.out.println("现在"+name+"处于"+this.getStatus());
         System.out.println("已经生长了5天，距离收获还有5天！");
     }
 }
--- august/ba0802/farm/Corn.java
+++ august/ba0802/farm/Corn.java
@@ -9,6 +9,7 @@ package august.ba0802.farm;
  */
 public class Corn extends Crop{
     public Corn(){
+/*
 
         name="玉米";
         this.setGroTime(8);
@@ -16,7 +17,16 @@ public class Corn extends Crop{
         this.setNumsFruit(200);
         this.setStatus(GROW);
         this.setHarvested(true);
+*/
 
+    }
+    public Corn(String name,int groTime,int havrsTime,int numsFruit,String status,boolean isHarvested,double money){
+        this.name=name;
+        this.setGroTime(groTime);
+        this.setHavrsTime(havrsTime);
+        this.setNumsFruit(numsFruit);
+        this.setStatus(status);
+        this.money=money;
     }
 
 /*
@@ -41,22 +51,30 @@ public class Corn extends Crop{
         this.money = money;
     }
 
+    //更换收割机
+    public void grafting(double cost){
+        if(this.money==cost){
+            System.out.println("更换收割机后，费用不变！");
+        }else {
+            this.money=cost;
+            System.out.println("更换收割机后，费用变为"+this.money);
+        }
+    }
 
     @Override
     public void show(){
-        /*
         System.out.println("*****作物特性*****");
         System.out.println("你种植了"+name+",");
         System.out.println("成长期"+this.getGroTime()+"天，采摘期"+this.getHavrsTime()+"天，" +
                 "产量"+this.getNumsFruit()+"斤，现在处于"+this.getStatus()+"，");
-        */
-        super.show();
+        //super.show();
         System.out.println(name+"属于农作物，收割机费用："+money+"元。");
     }
 
     @Override
     public void state() {
-        super.state();
+        /*super.state();*/
+        System.out.println("现在"+name+"处于"+this.getStatus());
         System.out.println("已经生长了7天，距离收获还有1天！");
     }
 }
