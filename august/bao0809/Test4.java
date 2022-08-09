8018d0378d52f3eeae29e410b146a0461740266b
CQ996 <chengqian8417@gmail.com>
2022-08-09 16:43:33 +0800

    模拟购物方法,抛出异常

---
+++ august/bao0809/Test4.java
@@ -0,0 +1,43 @@
+package august.bao0809;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName Test4
+ * @Description 模拟购物方法,抛出异常
+ * @Author CQ
+ * @Date 2022/8/9 16:21
+ * @Version 1.0
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int num;
+        double money;
+        //模拟购物方法（需要传入：购物商品数量，银行卡余额）
+        //内部逻辑：1、检查数量不能超出1份，如果超出，则抛出数量限制异常
+        //        2、银行卡余额不少于100元，如果少，则抛出余额不足异常
+
+        //测试：主方法调用并传递测试数据完成测试，如果产生异常，将捕获并给予温馨提示。
+        System.out.println("请输入你的购买数量和银行卡余额：");
+        num=in.nextInt();
+        money=in.nextDouble();
+            try {
+                shop(num,money);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+    }
+    public static void shop(int num,double money)throws Exception{
+        if(num>1){
+            Exception e1=new Exception("超出数量限制");
+            throw e1;
+        }
+        if(money<100){
+            Exception e1=new Exception("余额不足");
+            throw e1;
+        }
+
+    }
+}
