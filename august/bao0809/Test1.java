ae2956b6e79fef30981a1146874060e107d3df8b
CQ996 <chengqian8417@gmail.com>
2022-08-09 17:00:58 +0800

    了解 编译异常 和 运行异常

--- august/bao0809/Test1.java
+++ august/bao0809/Test1.java
@@ -27,8 +27,8 @@ public class Test1 {
         //演示运行异常，编码过程中，自动产生
         //int a=10/0;
         //演示运行异常，编码过程中，手动产生
-        //RuntimeException e1=new RuntimeException("电脑故障");
-        //throw e1;
+        RuntimeException e1=new RuntimeException("电脑故障");
+        throw e1;
 
         //总结，运行异常无需预处理，但是可以进行处理
 
