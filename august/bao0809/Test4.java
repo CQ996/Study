3ede5a048de55270ad5e666ff8f148945cdb6cdf
CQ996 <chengqian8417@gmail.com>
2022-08-09 17:34:11 +0800

    模拟输入成绩，自定义异常

--- august/bao0809/Test4.java
+++ august/bao0809/Test4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * @ClassName Test4
- * @Description 模拟购物方法,抛出异常
+ * @Description 模拟购物方法,抛出异常，自定义异常
  * @Author CQ
  * @Date 2022/8/9 16:21
  * @Version 1.0
@@ -19,25 +19,41 @@ public class Test4 {
         //        2、银行卡余额不少于100元，如果少，则抛出余额不足异常
 
         //测试：主方法调用并传递测试数据完成测试，如果产生异常，将捕获并给予温馨提示。
-        System.out.println("请输入你的购买数量和银行卡余额：");
+        System.out.println("请输入你的购买数量：");
         num=in.nextInt();
-        money=in.nextDouble();
-            try {
-                shop(num,money);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+        if (num >=1){
+            //这里我们可以通过构造器，设置信息
+            throw new YEBZException("超出数量限制");
+        }
+        System.out.println("没毛病！");
+
+//        System.out.println("请输入你的购买数量和银行卡余额：");
+//        num=in.nextInt();
+//        money=in.nextDouble();
+//            try {
+//                shop(num,money);
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
 
     }
-    public static void shop(int num,double money)throws Exception{
-        if(num>1){
-            Exception e1=new Exception("超出数量限制");
-            throw e1;
-        }
-        if(money<100){
-            Exception e1=new Exception("余额不足");
-            throw e1;
-        }
 
+//    public static void shop(int num,double money)throws Exception{
+//        if(num>1){
+//            Exception e1=new Exception("超出数量限制");
+//            throw e1;
+//        }
+//        if(money<100){
+//            Exception e1=new Exception("余额不足");
+//            throw e1;
+//        }
+//
+//    }
+
+    static class YEBZException extends RuntimeException {
+        public YEBZException(String message) {//构造器
+            super(message);
+        }
     }
+
 }
