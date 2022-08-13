e2a23255c025ed8e402002ce0c89278fff3c6ba8
CQ996 <chengqian8417@gmail.com>
2022-08-13 08:32:38 +0800

    ArrayList细节

---
+++ august/bao0811/list/ArrayListDetail.java
@@ -0,0 +1,27 @@
+package august.bao0811.list;
+
+import java.util.ArrayList;
+
+/**
+ * @ClassName ArrayListDetail
+ * @Description ArrayList细节
+ * @Author CQ
+ * @Date 2022/8/13 7:49
+ * @Version 1.0
+ */
+public class ArrayListDetail {
+    public static void main(String[] args) {
+        //ArrayListxian'c2不安全，如下源码方法中没有 synchronized 修饰（线程互斥）
+        /*
+        public boolean add(E e) {
+        ensureCapacityInternal(size + 1);  // Increments modCount!!
+        elementData[size++] = e;
+        return true;
+    }*/
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(null);
+        arrayList.add("jack");
+        //1.ArrayList可以放入空值，并且多个
+        System.out.println(arrayList);
+    }
+}
