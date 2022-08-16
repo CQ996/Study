4ba75bf752ed1c825c09d4b8b234e6092747a589
CQ996 <chengqian8417@gmail.com>
2022-08-16 10:17:57 +0800

    使用迭代器根据班级获取学生姓名

---
+++ august/bao0816/StInfor.java
@@ -0,0 +1,42 @@
+package august.bao0816;
+
+import java.util.*;
+
+/**
+ * @ClassName StInfor
+ * @Description 使用迭代器根据班级获取学生姓名
+ * @Author CQ
+ * @Date 2022/8/16 8:16
+ * @Version 1.0
+ */
+public class StInfor {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        ArrayList st1 = new ArrayList();
+        st1.add("张三丰");
+        st1.add("杨过");
+        st1.add("郭靖");
+        ArrayList st2 = new ArrayList();
+        st2.add("王二麻");
+        st2.add("杨广");
+        st2.add("李自成");
+        HashMap<String, ArrayList> stt = new HashMap<String, ArrayList>();
+        stt.put("三年级一班",st1);
+        stt.put("三年级二班",st2);
+        System.out.println("输入年级：");
+        String grade=in.next();
+        System.out.println(grade+"学生列表：");
+        ArrayList st= stt.get(grade);
+        Iterator it = st.iterator();
+        while (it.hasNext()) {
+            Object next =  it.next();
+            System.out.println(next);
+        }
+//        for (String key :keys) {
+//            ArrayList name = stt.get(key);
+//            System.out.println(name);
+//        }
+
+
+    }
+}
