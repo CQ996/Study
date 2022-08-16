46b4a9b7def7043c60f6cfbfc95869adf12ffae6
CQ996 <chengqian8417@gmail.com>
2022-08-16 16:28:03 +0800

    Collections :  集合操作工具类. 注: 提供了一些静态操作集合的方法.

---
+++ august/bao0816/map/Test4.java
@@ -0,0 +1,37 @@
+package august.bao0816.map;
+
+/**
+ * @ClassName Test4
+ * @Description Collections :  集合操作工具类. 注: 提供了一些静态操作集合的方法.
+ * @Author CQ
+ * @Date 2022/8/16 14:13
+ * @Version 1.0
+ */
+public class Test4 {
+    public static void main(String[] args) {
+
+
+        // Collections :  集合操作工具类. 注: 提供了一些静态操作集合的方法.
+
+        //演示:
+
+        //binarySearch()
+
+        //copy()
+
+        //reverse()
+
+        //sort()
+
+        //swap()
+
+        //synchronizedXXXX()
+
+        //unmodifiableXXXX()
+
+
+
+
+    }
+}
+
