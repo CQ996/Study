70396785b8830d3ba444b005dcf0bae6d1756a3d
CQ996 <chengqian8417@gmail.com>
2022-08-23 05:55:57 +0800

    多线程的创建方式二：实现Runnable任务接口  * 优点：只实现接口，可以继续继承类和实现接口，扩展性更强  * 缺点：编程多一层对象包装，如果线程有执行结果是不能直接返回的

--- august/bao0823/itheima_create/Runnable_Demo2.java
+++ august/bao0823/itheima_create/Runnable_Demo2.java
@@ -14,7 +14,7 @@ public class Runnable_Demo2 {
         //3.创建一个任务对象
         Runnable target = new MyRunnable();
         //4.把任务对象交给Thread处理
-        Thread t=new Thread(target);
+        Thread t=new Thread(target,"1号线程");
         //5.启动线程
         t.start();
 
@@ -30,7 +30,7 @@ class MyRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("子线程执行输出" + i);
+            System.out.println(Thread.currentThread().getName()+"输出" + i);
         }
     }
 }
