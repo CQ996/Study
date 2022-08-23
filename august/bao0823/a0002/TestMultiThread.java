2e7ae0fe17b8a77ec89d0f22ea15e84117ef5399
CQ996 <chengqian8417@gmail.com>
2022-08-23 11:38:37 +0800

    多线程打印0~100偶数

--- august/bao0823/a0002/TestMultiThread.java
+++ august/bao0823/a0002/TestMultiThread.java
@@ -20,6 +20,13 @@ public class TestMultiThread {
 }
 
 class MyThread extends Thread{
+    public MyThread() {
+    }
+
+    public MyThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
         for (int i = 0; i <= 100; i++) {
