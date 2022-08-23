0b6a6a610c93fcb2bb23e527aea3270a8d3ba690
CQ996 <chengqian8417@gmail.com>
2022-08-23 10:10:08 +0800

    多线程打印0~100偶数

---
+++ august/bao0823/a0002/TestMultiThread.java
@@ -0,0 +1,31 @@
+package august.bao0823.a0002;
+
+/**
+ * @ClassName TestMutilThready
+ * @Description 多线程打印0~100偶数
+ * @Author CQ
+ * @Date 2022/8/23 9:18
+ * @Version 1.0
+ */
+public class TestMultiThread {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.setName("乾坤大挪移：");
+        myThread.start();
+
+        MyThread myThread2 = new MyThread();
+        myThread2.setName("九阳神功：");
+        myThread2.start();
+    }
+}
+
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            if(i%2==0){
+                System.out.println(Thread.currentThread().getName()+"\n"+i);
+            }
+        }
+    }
+}
