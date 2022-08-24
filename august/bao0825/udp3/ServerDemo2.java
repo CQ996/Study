f734ffea6cc6096bf3af515a151cbc450d495911
CQ996 <chengqian8417@gmail.com>
2022-08-24 14:33:15 +0800

    UDP广播

---
+++ august/bao0825/udp3/ServerDemo2.java
@@ -0,0 +1,35 @@
+package august.bao0825.udp3;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+ * @ClassName DatagramPacket_
+ * @Description UDP广播
+ * @Author CQ
+ * @Date 2022/8/24 13:38
+ * @Version 1.0
+ */
+public class ServerDemo2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("=====服务端启动====");
+        //1.创建接收端对象：注册端口（人）
+        DatagramSocket socket = new DatagramSocket(9999);
+        //2.创建一个数据包对象接收数据
+        byte[] buffer = new byte[1024*64];
+        DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+
+        while (true) {
+            //3.等待接收数据
+            socket.receive(packet);
+
+            //4.取出数据
+            //读取多少倒多少
+            int len = packet.getLength();
+            String rs=new String(buffer,0,len);
+            System.out.println("收到了来自"+packet.getAddress()+",对方端口是"+packet.getPort()+"的消息"+rs);
+
+
+        }
+    }
+}
